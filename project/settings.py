db21650aabbbe3607d86417d5f7559e51dc57ac2
Suman-Return <Suman.745421@bimctu.edu.np>
2021-12-07 11:07:59 +0545

    s1

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '#sz40a+$&ogg(fng*cr0tzs%ldd=mfy#j*zf6xen@sozjpnbiz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','dms-pwnbot.herokuapp.com/']
 
 
 # Application definition
